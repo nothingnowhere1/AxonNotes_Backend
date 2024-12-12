b38547829a34d9e6bfae564c7a4a4b7003e5a5d2
nothing <khisametdinov@418team.com>
2024-12-12 13:32:03 +0300

    swagger

---
+++ src/app.constant.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_ENDPOINT: string = 'axonnotes';
+
+export const HOST: string = process.env.HOST || '127.0.0.1';
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SWAGGER_ENDPOINT } from './app.constant';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+
+	SwaggerSetup(app);
+	await app.listen(process.env.PORT ?? 3000);
 }
+
+function SwaggerSetup(app: INestApplication) {
+	if (!SWAGGER_ENDPOINT) return false;
+
+	const config = new DocumentBuilder()
+		.setTitle('AxonNotes')
+		.setDescription('AxonNotes API')
+		.setVersion('0.1')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup(SWAGGER_ENDPOINT, app, document);
+	return SWAGGER_ENDPOINT;
+}
+
 bootstrap();
