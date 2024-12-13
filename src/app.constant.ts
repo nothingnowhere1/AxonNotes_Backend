7c2b15bf2b7f3a45d6082699670a12278e4614e9
nothing <khisametdinov@418team.com>
2024-12-13 16:29:07 +0300

    typeORM

--- src/app.constant.ts
+++ src/app.constant.ts
@@ -1,11 +1,9 @@
-export const POSTGRES_HOST: string = process.env.DB_HOST || '127.0.0.1';
-export const POSTGRES_PORT: number = +(process.env.DB_PORT) || 5432;
-export const POSTGRES_USER: string = process.env.DB_USERNAME || 'postgres';
-export const POSTGRES_PASSWORD: string = process.env.DB_PASSWORD || 'qwerty';
-export const POSTGRES_DB: string = process.env.DB_NAME || 'postgres';
+export const POSTGRES_HOST: string = process.env.DB_HOST || '127.0.0.1'
+export const POSTGRES_PORT: number = +process.env.DB_PORT || 5432
+export const POSTGRES_USER: string = process.env.DB_USERNAME || 'postgres'
+export const POSTGRES_PASSWORD: string = process.env.DB_PASSWORD || 'qwerty'
+export const POSTGRES_DB: string = process.env.DB_NAME || "postgres"
 
+export const SWAGGER_ENDPOINT: string = 'axonnotes'
 
-export const SWAGGER_ENDPOINT: string = 'axonnotes';
-
-export const HOST: string = process.env.HOST || '127.0.0.1';
-
+export const HOST: string = process.env.HOST || '127.0.0.1'
